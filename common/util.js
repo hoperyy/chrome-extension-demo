cfbc378f98560dac173f54344b2d2473b4b46285
hoperyy <antfootmail@gmail.com>
2018-12-05 19:02:29 +0800

    [feat] update

---
+++ common/util.js
@@ -0,0 +1,27 @@
+/**
+ * @author 刘远洋
+ * @class 工具类
+ */
+
+var ChromeExtention_util = {
+    addCss: function(href, id) {
+        $('<link href="' + chrome.extension.getURL(href) + '" id="' + id + '" rel="stylesheet" type="text/css" />').appendTo('head');
+    },
+    addJs: function (src, id) {
+      $('<script src="' + chrome.extension.getURL(src) + '" id="' + id + '"></script>').appendTo('body');
+    },
+    removeCss: function(id) {
+      $('head').find(id).remove();
+    },
+    isSupportLocalStorage: function() {
+      try {
+        window.localStorage.setItem('ChromeExtention_FeHelper_testLs', '1');
+        window.localStorage.getItem('ChromeExtention_FeHelper_testLs');
+        window.localStorage.removeItem('ChromeExtention_FeHelper_testLs');
+        return true;
+      }
+      catch (err) {
+        return false;
+      }
+    }
+};
--- popup.js
+++ popup.js
@@ -1,23 +1,114 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  var resultsButton = document.getElementById('getResults');
-  resultsButton.onclick = getResults;
-});
+/**
+ * FeHelper弹出（下拉）页面
+ * @author liuyuanyang
+ */
+$(function () {
 
-function getResults() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'checkForWord' }, function (response) {
-          if (response) {
-            showResults(response.results);
-          } else {
-            showResults();
-          }
-        }
-      );
+    function Popup() {
+        this.getDeveloper.init();
+        this.visualCompare.init();
     }
-  );
-}
 
-function showResults(results) {
-  var resultsElement = document.getElementById('results');
-  resultsElement.innerText = results ? 'This page uses jQuery' : 'This page does NOT use jQuery';
-}
+    $.extend(Popup.prototype, {
+        visualCompare: {
+            init: function () {
+                this.STATE_ON_TEXT = 'on';
+                this.STATE_OFF_TEXT = 'off';
+                this.STATE_UNDEFINED = 'localStorage unavailable';
+
+                this.addEvent();
+
+                var _this = this;
+
+                // 从 localStorage 获取状态
+                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        action: 'visual-compare-get-state'
+                    }, function (response) {
+                        var $target = jQuery('.J_VisualCompare');
+
+                        if (!response) {
+                            $target.find('.J_State').text('not available');
+                            return;
+                        }
+
+                        var state = response.state;
+
+                        if (!state || state == 'off') {
+                            $target.find('.J_State').text(_this.STATE_OFF_TEXT);
+                        }
+
+                        if (state == 'on') {
+                            $target.find('.J_State').text(_this.STATE_ON_TEXT);
+                        }
+
+                        if (state == 'undefined') {
+                            $target.find('.J_State').text(_this.STATE_UNDEFINED);
+                        }
+
+                    });
+                });
+
+            },
+
+            addEvent: function () {
+                var _this = this;
+
+                // 菜单点击以后执行的动作
+                jQuery('.J_VisualCompare').click(function (e) {
+                    var $target = $(this);
+                    var $state = $target.find('.J_State');
+                    var currentState = $state.text();
+
+                    if (currentState !== 'on' && currentState !== 'off') {
+                        return;
+                    }
+
+                    if (currentState == 'off') {
+                        $state.text(_this.STATE_ON_TEXT);
+                    }
+
+                    if (currentState == 'on') {
+                        $state.text(_this.STATE_OFF_TEXT);
+                    }
+
+                    // 同步到 localstorage
+                    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                            action: 'visual-compare-set-state',
+                            targetState: currentState === 'on' ? 'off' : 'on'
+                        }, function (response) {
+                            console.log('设置 localstorage 结果: ', response.msg);
+                        });
+                    });
+                    // window.close();
+                });
+            },
+        },
+
+        getDeveloper: {
+            init: function () {
+                this.addEvent();
+                var _this = this;
+            },
+
+            addEvent: function () {
+                var _this = this;
+
+                // 菜单点击以后执行的动作
+                jQuery('.J_GetDeveloper').click(function (e) {
+                    // 通知页面 js 获取开发者信息
+                    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                            action: 'get-developer-run'
+                        }, function (response) {
+
+                        });
+                    });
+                });
+            },
+        }
+    });
+
+    new Popup();
+});
